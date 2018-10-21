63d0d7cc202396099cb5fe90efcf1a0c1ab22793
Abhishek Gupta <agupt222@asu.edu>
2018-10-20 17:06:38 -0700

    Deployable Code Commit.
    Front end is ready to be deployed on a firebase hosting server.

--- frontend/hamburg/src/components/SearchResults.vue
+++ frontend/hamburg/src/components/SearchResults.vue
@@ -1,25 +1,33 @@
 <template>
-    <section   v-if="error.length" class="section">
-        <div class="notification is-danger">
-            {{this.error}}
-        </div>
-    </section>
-    <section  v-else class="section">
-        <article class="media" v-for="result in results">
-            <figure class="media-left">
-                <p class="image is-256x256">
-                    <img v-bind:src="posterBase+result.poster_path ">
-                </p>
-            </figure>
-            <div class="media-content ">
-                <div class="content">
-                    <a><h4 class="title is-4 is-spaced">{{ result.title }}</h4></a>
-                    <p class="subtitle is-6 is-spaced">{{ result.overview }}</p>
-                    <p class="subtitle is-6 is-spaced"> {{result.release_date}}</p>
+    <div class="main container-fluid">
+        <div class="holder">  
+            <section   v-if="error.length" class="section">
+                <div class="notification is-danger">
+                    <b-card class="bucket">
+                    <b-media tag="li" class="my-4">
+                        <h2 class="mt-0 mt-1"><b>{{ "Error" }}</b></h2>
+                        <p>{{ this.error }}</p>
+                    </b-media>
+                    </b-card>
                 </div>
-            </div>
-        </article>
-    </section>
+            </section>
+            <section  v-else class="section">
+                <ul class="list-unstyled">
+                    <b-link class="movieLink" href="#" v-for="(result, index) in results" :key="index">
+                    <b-card class="bucket">
+                        <b-media tag="li" class="my-4">
+                        <b-img-lazy slot="aside" v-bind:src="posterBase+result.poster_path" 
+                            height="256" width="170" blank-src="@/assets/alt.png" blank-color="#bbb" alt="alt"/>
+                        <h5 class="mt-0 mt-1"><b>{{ result.title }}</b></h5>
+                        <p class='releaseDate'>{{ result.release_date }}</p>
+                        <p>{{ result.overview }}</p>
+                        </b-media>
+                    </b-card>  
+                    </b-link>
+                </ul>
+            </section>
+        </div>    
+    </div>
 </template>
 <script>
    export default {
@@ -33,3 +41,30 @@
        }
    }
 </script>
+
+<style>
+.main {
+  position: relative;
+  margin: 105px auto 10px;
+  align-items: center;
+  height: 100%;
+  width: 60%;
+  z-index: 2;
+}
+.bucket {
+  padding: 10px;
+  margin: 20px auto;
+  box-shadow: 0px 0px 40px lightgray;
+}
+.bucket:hover {
+  box-shadow: 0px 0px 20px darkgrey;
+}
+.movieLink {
+  text-decoration: none;
+  color: #333;
+}
+.movieLink:hover {
+  text-decoration: none;
+  color: #07b;
+}
+</style>
